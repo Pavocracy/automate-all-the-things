af509f9ed53591b1bd9be03e61a33ddab308be87
Pavocracy <pavocracy@pm.me>
2022-06-30 22:28:58 +1000

    formatting

--- get_and_solve_coding_challenges/get_coding_challenges.py
+++ get_and_solve_coding_challenges/get_coding_challenges.py
@@ -1,12 +1,10 @@
 from leetscraper import Leetscraper
 
 if __name__ == "__main__":
-    websites = [
-        "leetcode.com",
-        "projecteuler.net",
-        "codechef.com",
-        "hackerrank.com",
-        "codewars.com",
-    ]
+    websites = ["leetcode.com",
+                "projecteuler.net",
+                "codechef.com",
+                "hackerrank.com",
+                "codewars.com"]
     for site in websites:
         Leetscraper(website_name=site)
